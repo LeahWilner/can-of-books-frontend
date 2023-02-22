28c10bd1e9d60713eb51cd3ae13c441760365117
SHargus <87950738+harguss@users.noreply.github.com>
2023-02-22 15:48:05 -0600

    update and format create book component

--- src/components/CreateBook.js
+++ src/components/CreateBook.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import { Container } from 'react-bootstrap';
-import BookFormModal from '../components/BookFormModel';
-// import axios from "axios";
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Container } from "react-bootstrap";
+import BookFormModal from "../components/BookFormModel";
+
 
 class CreateBook extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            showModal: false
+            showModal: false,
         };
     }
 
     handleShowModal = () => {
-        this.setState({ showModal: true });
-    }
-
+        this.setState({
+            showModal: true
+        });
+    };
     handleHideModal = () => {
-        this.setState({ showModal: false });
-    }
-
+        this.setState({
+            showModal: false
+        });
+    };
     handleSubmit = (book) => {
-        // Call the parent component's onSubmit function with the book object
         this.props.onSubmit(book);
-    }
+    };
 
     render() {
         return (
             <Container className="mt-5">
                 <Button variant="outline-secondary" onClick={
                     () => this.handleShowModal()}>Add Book</Button>
-               
-             
-              
-               
-               
                 <BookFormModal
                     show={this.state.showModal}
-                    handleBookSubmit={this.props.handleBookSubmit}
+                    handleBookSubmit=
+                    {this.props.handleBookSubmit}
                     onHide={this.handleHideModal}
                 />
             </Container>
