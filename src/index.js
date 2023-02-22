f4cab02ef97aec252583da9ff7af8bcb94f23f15
SHargus <87950738+harguss@users.noreply.github.com>
2023-02-22 14:18:49 -0600

    updated indax imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './App.js';
 import About from './components/About';
-import Header from './components/Header.js';
+import Header from './components/Header/Header.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   createBrowserRouter,
